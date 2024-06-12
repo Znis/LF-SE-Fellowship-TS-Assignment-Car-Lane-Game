b478b4bc7506d73f05e3b9bcf4f97fc77cc8fc72
Znis <metwayana@gmail.com>
2024-06-12 09:27:57 +0545

    update: adds comments

--- src/state-variables.ts
+++ src/state-variables.ts
@@ -1,38 +1,36 @@
 import Car from "./components/car";
-
+//game state variables that is accessed globally
 export enum GameState {
-    welcomeScreen = "welcomeScreen",
-    initialisation = "initialisation",
-    running = "running",
-    paused = "paused",
-    resume = "resume",
-    gameOver = "gameOver",
-    restart = "restart",
+  welcomeScreen = "welcomeScreen",
+  initialisation = "initialisation",
+  running = "running",
+  paused = "paused",
+  resume = "resume",
+  gameOver = "gameOver",
+  restart = "restart",
 }
 
 type stateVariables = {
-    prevValidRandomYposition: number,
-    collisionOccurred: boolean,
-    reqAnimFrame: number;
-    opponentCarArray: Car[];
-    playerCar: Car;
-    score: number;
-    gameOver: boolean,
-    gameState: GameState,
-    highScore: number,
-    isTransitionActive: boolean,
-
-}
-export const stateVariables ={
-    prevValidRandomYposition: 0,
-    collisionOccurred: false,
-    reqAnimFrame: 0,
-    opponentCarArray: [] as Car[],
-    playerCar: {} as Car,
-    score: 0,
-    highScore: 0,
-    gameOver: false,
-    gameState: GameState.welcomeScreen,
-    isTransitionActive: false,
-
-} 
+  prevValidRandomYposition: number;
+  collisionOccurred: boolean;
+  reqAnimFrame: number;
+  opponentCarArray: Car[];
+  playerCar: Car;
+  score: number;
+  gameOver: boolean;
+  gameState: GameState;
+  highScore: number;
+  isTransitionActive: boolean;
+};
+export const stateVariables = {
+  prevValidRandomYposition: 0,
+  collisionOccurred: false,
+  reqAnimFrame: 0,
+  opponentCarArray: [] as Car[],
+  playerCar: {} as Car,
+  score: 0,
+  highScore: 0,
+  gameOver: false,
+  gameState: GameState.welcomeScreen,
+  isTransitionActive: false,
+};
